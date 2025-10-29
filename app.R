4355f644f7fdb2d5c7eb8549413f5a97e837abd0
Ryan Parks <ryan.j.parks1996@gmail.com>
2025-10-29 10:49:48 -0400

    Add files via upload
    Corrected code to now run remotely. All references to a saved data frame (was used for testing during an earlier build) now point to the file upload wizard. Provided the correct file is uploaded, the code will not have issues finding variables from the data.
    
    Additionally, uploaded a most recent copy of the weekly TPT report (10/27/25)

--- app.R
+++ app.R
@@ -22,14 +22,23 @@ ui <- page_fluid(
     fileInput("upload", NULL, accept = ".xlsx")
   ),
   
-  layout_columns(
-    col_widths = 4,
+  #layout_columns(
+    #col_widths = 4,
     # Select region----
-    selectInput(
-      "region",
-      label = "Select region",
-      choices = c("All", sort(unique(TPT_Report$Region)))
-    ),
+    #selectInput(
+      #"region",
+      #label = "Select region",
+     # choices = c("All", sort(unique(TPT_Report$Region)))
+    #),
+    
+    layout_columns(
+      col_widths = 4,
+      # Select region (dynamic ui)----
+      selectInput(
+        "region",
+        label = "Select region",
+        choices = NULL
+      ),
     
     # Select district (dynamic ui)----
     selectInput(
@@ -46,30 +55,41 @@ ui <- page_fluid(
     ),
     
     # Select time frame----
+    #sliderInput(
+      #"timeRange",
+     # label = "Select range of weeks",
+    #  min = 1,
+      # Each week the max number of weeks will increase
+      #max = max(TPT_Report$Week),
+     # value = c(1, max(TPT_Report$Week))
+    #),
+    # Select time frame (dynamic ui)----
     sliderInput(
       "timeRange",
       label = "Select range of weeks",
       min = 1,
       # Each week the max number of weeks will increase
-      max = max(TPT_Report$Week),
-      value = c(1, max(TPT_Report$Week))
+      max = 52,
+      value = c(1, 52)
     ),
     
     # Toggle to select weekly averages across all selected stores----
     checkboxInput(
       "allSites",
-      "Plot median of all stores",
+      label = "Plot median of all stores",
       value = FALSE
     ),
+    
     # Toggle to select weekly averages across all selected stores----
+    # Currently disabled 
     checkboxInput(
       "monthly",
-      "Monthly Averages (CURRENTLY DEACTIVATED)",
+      label = "Monthly Averages (CURRENTLY DEACTIVATED)",
       value = FALSE
     )
   ),
   
-  
+  tableOutput("head"),
   plotOutput("plot"),
   verbatimTextOutput("summary")
 )
@@ -78,23 +98,38 @@ ui <- page_fluid(
 # Define server logic----
 server <- function(input, output) {
   
+  # File upload wizard
   data <- reactive({
     req(input$upload)
-    if (input$monthly) {
-      file_convert_to_tidy(input$upload$name, "Week to Week")
-    } else {
-      file_convert_to_tidy(input$upload$name, "Period to Period")
+    
+    filepath <- input$upload$datapath
+    if (!input$monthly) {
+      file_convert_to_tidy(filepath, "Week to Week")
+    } else { # Disabled function by always choosing 'Week to Week'
+      #file_convert_to_tidy(filepath, "Period to Period")
+      file_convert_to_tidy(filepath, "Week to Week")
     }
   })
   
-  # Interactive drill down from region -> district -> site---- 
+  
+  # Interactive drill down from region -> district -> site---------------------- 
+  
+  ## REGION ##
+  # Get list of Regions and update choices in program
+  observeEvent(data(), {
+    choices <- unique(data()$Region)
+    updateSelectInput(inputId = "region", choices = c("All", sort(choices)))
+  })
+  
   region <- reactive({
     req(input$upload)
     if (input$region != "All") {
-      filter(TPT_Report, Region == input$region)
-    } else return(TPT_Report)
-  })
+      filter(data(), Region == input$region)
+    } else return(data())
+  }) 
   
+  ## DISTRICT ##
+  # Get list of DMs in a selected Region and update choices in program
   observeEvent(region(), {
     choices <- unique(region()$DM)
     updateSelectInput(inputId = "district", choices = c("All", sort(choices)))
@@ -102,69 +137,98 @@ server <- function(input, output) {
   
   district <- reactive({
     req(input$district)
+    # Filter data by selected DM 
     if (input$district != "All") {
-      filter(region(), DM == input$district)
-    } else return(region()) # Returns rows from all DMs in a selected region
+      filter(region(), DM == input$district) 
+    } else return(region()) 
   })
   
+  ## SITE ##
+  # Get list of Sites in a selected Region/District and update choices in program
   observeEvent(district(), {
-    choices <- unique(district()$Site.name)
+    choices <- unique(district()$`Site name`)  
     updateSelectInput(inputId = "site", choices = c("All", sort(choices)))
   })
   
-  # Final step of drill down.
   site <- reactive({
     req(input$site)
+    #Filter data by selected Site
     if (input$site != "All") {
-      filter(district(), Site.name == input$site)
+      filter(district(), `Site name` == input$site) 
     } else return(district())
   })
+  # End of drill down-----------------------------------------------------------
+  
   
   averageAllStores <- reactive({
     weeklyData <- site()
     if (input$allSites) {
       weeklyData <- weeklyData %>%
         group_by(Week) %>%
-        mutate(TPT = median(TPT))
+        mutate(TPT = median(TPT)) # Median used due to large outliers
     } 
     return(weeklyData)
   })
   
   
+  # Add column to data to indicate if TPT is out of range
+  #(Used for color coding the plot)
+  flaggedTPT <- reactive({
+    averageAllStores() %>%
+      mutate(color_condition = ifelse(!(TPT >= 3.11 | TPT <= 2.89),"red", "blue"))
+  })
+  
+  # Adjust time range max value
+  observeEvent(data(), {
+    maxWeek <- max(data()$Week)
+    updateSliderInput(inputId = "timeRange", max = maxWeek)
+  })
+
   output$plot <- renderPlot({
     req(input$upload)
-    
-    selectedData <- averageAllStores() %>%
+    #filtering data by time range selected
+    selectedData <- flaggedTPT() %>%
       filter(Week >= input$timeRange[1] &
                Week <= input$timeRange[2])
     
-    
     # Create data frame that contains horizontal line of 3 (goal TPT)
+    # (Used in plot(s) as reference line)
     goal <- data.frame(yintercept=3, Goal = "3.0 TPT")
+    
     # Create ggplot object----
-    TPT_Plot <- ggplot(data=selectedData, aes(x=factor(Week), y=TPT))
+    TPT_Plot <- ggplot(data = selectedData, 
+                       aes(x = factor(Week), 
+                           y = TPT,
+                           color = color_condition)) +
+      scale_color_discrete(labels = c("red" = "Outside range", 
+                                      "blue" = "Inside range"))
     
     # Plot line plot for averages and simple scatter plot otherwise
     if (input$allSites) {
-      TPT_Plot <- ggplot(data=selectedData, aes(x=Week, y=TPT))
+      TPT_Plot <- ggplot(data=selectedData, 
+                         aes(x = Week, 
+                             y = TPT))
+        
       TPT_Plot <- TPT_Plot + geom_line() +
-        geom_point(aes(colour = TPT))
+        geom_point(aes(color=color_condition), size = 3) +
+        scale_color_discrete(labels = c("red" = "Outside range", 
+                                        "blue" = "Inside range"))
     } else {
-      TPT_Plot <- TPT_Plot + geom_point(aes(color=TPT))
+      TPT_Plot <- TPT_Plot + geom_point()
     }
     
     TPT_Plot <- TPT_Plot +
-      scale_color_gradient2(low="black", mid="purple", high="red", midpoint=3) +
       geom_hline(aes(yintercept=yintercept, linetype="3.0"), goal) + 
-      scale_linetype_manual(name = "Goal",
-                            values = 2,) +
-      labs(title = "Weekly TPT", 
+      scale_linetype_manual(name = "Goal", values = "dashed") +
+      labs(title = "Weekly TPT",
+           x = "Week",
+           color = "TPT",
            subtitle = paste("Region:", input$region, 
                             "\t \t District:", input$district, 
                             "\t \t Site:", input$site,
-                            "\t \t Median TPT:", input$allSites),
-           x="Week",
-           color = "TPT")
+                             "\t \t Median TPT:", input$allSites)
+           ) 
+    
     TPT_Plot
   })
   
@@ -179,7 +243,8 @@ server <- function(input, output) {
     dataset <- site() %>%
       filter(Week >= input$timeRange[1] &
                Week <= input$timeRange[2]) %>%
-      select(TPT) 
+        select(TPT)
+    
     summary(dataset)
   })
 }
